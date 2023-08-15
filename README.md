bb5c83d1e36b11b9025d92a1d5c711a83270b927
Vitor Amaral <vitor.works@hotmail.com>
2023-08-15 10:39:22 +0100

    chore: improve examples

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
-# examples
+![Basestack Pattern Background](https://i.imgur.com/Cund9sW.jpg)
 
-Example projects for Basestack
+# Basestack Platform Examples
+
+**Basestack** is an open-source stack designed specifically for developers and startups. It offers a suite of tools, including Feature Flags, with additional tools such as Feedback, Forms, and more on the horizon. Our goal is to empower you in building great products.
+
+#### Tools
+
+- ⛳ Feature Flags (beta) [Docs](https://docs.basestack.co/feature-flags) | [Website](https://basestack.co/)
+- 📄 Forms (Planned)
+- 💬 Feedback (Planned)
+  
+## Help & Support
+
+If you need assistance with anything, whether it's a new feature, a bug, a guide, or if you'd like to contribute feedback, don't hesitate to [open a discussion](https://github.com/basestack-co/basestack/discussions).
+
+## Deployment & Self-hosting
+
+Want to self-host Basestack Tools on your server? Explore the [self-hosting installation instructions](https://docs.basestack.co/feature-flags/deployment) for detailed guidance.
+
+## How to Contribute
+
+First and foremost, thank you for dedicating your time to enhance the platform. We warmly welcome all contributions. To get started, please review the [Contribute guidelines](https://docs.basestack.co/contributing). We appreciate your support in making our platform even better!
--- feature-flags/react-js/src/App.js
+++ feature-flags/react-js/src/App.js
@@ -1,75 +1,18 @@
 // BaseStack Flags SDK
-import {
-  useFlag,
-  useFlagAsync,
-  Flag,
-  FlagAsync,
-} from "@basestack/flags-react-sdk";
+import { useFlag } from "@basestack/flags-react-sdk";
 
 const Header = () => {
-  const { enabled, payload } = useFlag("header_v2");
+  const { enabled, payload } = useFlag("header");
 
   if (!enabled) return null;
 
   return <header style={{ backgroundColor: payload.color }}>Header</header>;
 };
 
-const Footer = () => {
-  const footerFlag = useFlag("footer_v2");
-  const footerFlagServer = useFlagAsync("footer_v2");
-
-  return (
-    <footer>
-      {footerFlag.enabled ? <p>New Footer Text</p> : <p>Old Footer Text</p>}
-
-      {!footerFlagServer.isLoading && footerFlagServer.flag.enabled && (
-        <div>
-          This feature is waiting for the server response to check if we can
-          enabled
-        </div>
-      )}
-    </footer>
-  );
-};
-
-const Body = () => {
-  return (
-    <section>
-      <Flag name="new_chat">
-        <p>BaseStack React Flags SDK</p>
-      </Flag>
-      <br />
-      <Flag
-        name="new_chat"
-        render={(flag) => (
-          <p style={{ color: flag.payload.color }}>
-            BaseStack React Flags SDK with Color Payload
-          </p>
-        )}
-      />
-      <br />
-      <FlagAsync name="new_chat">
-        <p>FlagAsync</p>
-      </FlagAsync>
-      <br />
-      <FlagAsync
-        name="new_chat"
-        render={(flag) => (
-          <p style={{ color: flag.payload.color }}>
-            FlagAsync with Color Payload
-          </p>
-        )}
-      />
-    </section>
-  );
-};
-
 function App() {
   return (
     <div className="App">
       <Header />
-      <Body />
-      <Footer />
     </div>
   );
 }
